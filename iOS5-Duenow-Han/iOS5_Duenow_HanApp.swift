6ea24ffd05f529104db2c3dfb0a89b055cc86c34
snahn2209 <106750285+snahn2209@users.noreply.github.com>
2023-12-12 10:30:34 +0100

    Initial Commit

---
+++ iOS5-Duenow-Han/iOS5_Duenow_HanApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS5_Duenow_HanApp.swift
+//  iOS5-Duenow-Han
+//
+//  Created by Sina Han on 12.12.23.
+//
+
+import SwiftUI
+
+@main
+struct iOS5_Duenow_HanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
